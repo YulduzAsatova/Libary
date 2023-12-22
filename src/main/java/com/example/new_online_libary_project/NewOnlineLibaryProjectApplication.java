b46d1ce07c9bae9aabc62616df79d3e63d694e6e
YulduzAsatova <asatovayulduz87@gmail.com>
2023-12-22 21:35:43 +0500

    new project open

---
+++ src/main/java/com/example/new_online_libary_project/NewOnlineLibaryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_online_libary_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewOnlineLibaryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewOnlineLibaryProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/new_online_libary_project/NewOnlineLibaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_online_libary_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewOnlineLibaryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
